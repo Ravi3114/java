df074e1a49d86bbb1b1a20f1c292d8b01e4b8cef
Ravi3114 <guggilapuravi3114@gmail.com>
2018-09-17 17:54:28 +0530

    Static variables out side the class was removed and each method operation is improved for reusability of methods

--- IntPali.java
+++ IntPali.java
@@ -1,19 +1,37 @@
-import static java.lang.System.*;
 import java.util.Scanner;
 public class IntPali{
-	public static void main(String[]args){
-		Scanner intObj=new Scanner(System.in);
-		int originNum=intObj.nextInt();
+	/* reverse the number*/
+	public static int reverseInt(int inNum){
+		int originalNum=inNum;
 		int rem=0;
-		int revNum=0;
-		for(int i=originNum;i>1;i=i/10){
+		int reverseNum1=0;
+		for(int i=originalNum;i>1;i=i/10){
 			rem=i%10;
-			revNum=revNum*10+rem;
+			reverseNum1=reverseNum1*10+rem;
 		}
-		if(originNum==revNum){
-			out.println(originNum+" "+"is a palindrome");
+		return reverseNum1;
+	}
+	/* test whether the original Number and reversed number are same or not */
+	public static boolean areBothSame(int originalNum1,int reverseNum2){
+		reverseNum2=reverseInt(originalNum1);
+		if(originalNum1==reverseNum2){
+			return true;
 		}else{
-			out.println(originNum+" "+"is not a palindrome");
+			return false;
 		}
+	
+	}
+	/*Main method has the Stdin*/
+	public static void main(String[]args){
+		Scanner newInt=new Scanner(System.in);
+		int p=newInt.nextInt();
+		int q=reverseInt(p);
+		 boolean testResult=areBothSame(p,q);
+		 if(testResult==true){
+			 System.out.println(p+" "+"is a Palindrome");
+		 }else{
+			 System.out.println(p+" "+"is not a palindrome");
+		 }
 	}
 }
+	
